a2677b36f3f32da483cab8e618e9dd5b58a00126
Mahonri Maia <mmaia@uolinc.com>
2024-07-13 18:41:59 -0300

    Criação dos endpoints de Categorias

--- workout_api/centro_treinamento/models.py
+++ workout_api/centro_treinamento/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from workout_api.contrib.models import BaseModel
+from workout_api.atleta.models import AtletaModel
 
 
 class CentroTreinamentoModel(BaseModel):
@@ -10,4 +11,4 @@ class CentroTreinamentoModel(BaseModel):
     nome: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     endereco: Mapped[str] = mapped_column(String(60), nullable=False)
     proprietario: Mapped[str] = mapped_column(String(30), nullable=False)
-    atleta: Mapped['AtletaModel'] = relationship(back_populates='centros_treinamento')
+    atleta: Mapped['AtletaModel'] = relationship(back_populates='centro_treinamento')
