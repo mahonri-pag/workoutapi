7396d0ca3fa527ec376111e39174ff1216f0ff7e
Mahonri Maia <mmaia@uolinc.com>
2024-07-13 19:09:00 -0300

    Criação dos endpoints de Centros de treinamento

--- workout_api/centro_treinamento/schemas.py
+++ workout_api/centro_treinamento/schemas.py
@@ -1,10 +1,18 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import UUID4, Field
 from workout_api.contrib.schemas import BaseSchema
 
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
     endereco: Annotated[str, Field(description='Endereço do centro de treinamento', example='Rua dos Marombas, 157', max_length=60)]
     proprietario: Annotated[str, Field(description='Proprietário do centro de treinamento', example='Hércules Herculano', max_length=30)]
+
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[str, Field(description='Nome do centro de treinamento', example='CT King', max_length=20)]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description='Identificador de centro de treinamento')]
